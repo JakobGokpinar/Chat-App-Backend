1090c2447bf6581c5365cb36a420eb2ee4d8d6ed
AhmetGkp <48160017+AhmetGkp@users.noreply.github.com>
2020-07-22 01:01:48 +0200

    fdskj

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-include('adodb.inc.php');
+/*include('adodb.inc.php');
 //echo "hi man";
 
 $cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
@@ -20,6 +20,39 @@ $db->Connect(
 $ADODB_FETCH_MODE = ADODB_FETCH_ASSOC;
 
 // Use UTF-8
-$db->EXECUTE("set names 'utf8'"); 
+$db->EXECUTE("set names 'utf8'"); */
+
+//Get Heroku ClearDB connection information
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server   = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db       = substr($cleardb_url["path"],1);
+
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+    'dsn'    => '',
+    'hostname' => $cleardb_server,
+    'username' => $cleardb_username,
+    'password' => $cleardb_password,
+    'database' => $cleardb_db,
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
 
 ?>
