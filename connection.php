77aacfe4b688f0a1f6b727a243debb43b76ca685
AhmetGkp <48160017+AhmetGkp@users.noreply.github.com>
2020-07-22 01:14:15 +0200

    fkdsj

--- connection.php
+++ connection.php
@@ -1,6 +1,7 @@
 <?php
 
-    $connection = mysqli_connect('localhost:3306', 'root', '', 'goksoft chat app');
+    #$connection = mysqli_connect('localhost:3306', 'root', '', 'goksoft chat app');
+    $connection = mysqli_connect('us-cdbr-east-02.cleardb.com:3306', 'bfca2adbd98d3d', 'ebebb0b8', 'heroku_77553a4fbd53445');
     if (session_status() == PHP_SESSION_NONE) {
         session_start();
     }
