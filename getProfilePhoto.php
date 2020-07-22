9c30d4e7e75d3ad0a9712020d323a2af5aa5b598
AhmetGkp <48160017+AhmetGkp@users.noreply.github.com>
2020-07-22 07:56:41 +0200

    fdjk

--- getProfilePhoto.php
+++ getProfilePhoto.php
@@ -13,7 +13,7 @@
         echo "not found";
         exit();
     }
-    $rawDir = "users\\";
+    $rawDir = "users/";
     $directory = $rawDir . $username;
 
     $images = null;
@@ -34,7 +34,7 @@
     if (!file_exists($directory)) {   
         $images = glob($rawDir."default.jpg", GLOB_BRACE);
     }else{ 
-        $images = glob($directory. "\\*.{jpg,png,bmp}", GLOB_BRACE);
+        $images = glob($directory. "/*.{jpg,png,bmp}", GLOB_BRACE);
     }
     $images = $images[0];
 
