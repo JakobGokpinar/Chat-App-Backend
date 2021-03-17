6a2ba28e482aecacd44f7bad9439e05f08f7d08d
unknown <ahmettabar2003@gmail.com>
2021-03-17 20:04:18 +0100

    update

--- changePhoto.php
+++ changePhoto.php
@@ -2,11 +2,37 @@
     require 'connection.php';
     require 'checklog.php';
 
-    $username = mysqli_real_escape_string($connection, $username);
-    $result = mysqli_query($connection, "CALL changeProfilePhoto('$username','$blocked')");
-    if($result !== false){
-        echo "photo changed";
-    } else {
-        echo "photo not changed";
-    }
+    $status = $statusMsg = ''; 
+    $status = 'error'; 
+
+    if(!empty($_FILES["photo"]["name"])) { 
+        // Get file info 
+        $fileName = basename($_FILES["photo"]["name"]); 
+        $fileType = pathinfo($fileName, PATHINFO_EXTENSION); 
+         
+        // Allow certain file formats 
+        $allowTypes = array('jpg','png','jpeg','gif'); 
+        if(in_array($fileType, $allowTypes)){ 
+            $image = $_FILES['photo']['tmp_name']; 
+            $imgContent = addslashes(file_get_contents($image)); 
+         
+            // Insert image content into database 
+            $insert = $connection->query("UPDATE users SET photo = '$imgContent' WHERE username='$username'"); 
+            //$insert = true;
+            if($insert){ 
+                $status = 'success'; 
+                $statusMsg = "File uploaded successfully."; 
+            }else{ 
+                $statusMsg = "File upload failed, please try again."; 
+            }  
+        }else{ 
+            $statusMsg = 'Sorry, only JPG, JPEG, PNG, & GIF files are allowed to upload.'; 
+        } 
+    }else{ 
+        $statusMsg = 'Please select an image file to upload.'; 
+    } 
+ 
+ 
+// Display status message 
+echo $statusMsg;
 ?>
