ad677da16c2e670be5719d8392dca87a3a2c4bbd
unknown <ahmettabar2003@gmail.com>
2021-03-28 19:21:16 +0200

    fds

--- getMessage.php
+++ getMessage.php
@@ -9,15 +9,19 @@
     //call getMessage() procedure where sender is logged in user and receiver is the chatted friend that taken with post request.
     $result = mysqli_query($connection, "CALL getMessage('$username', '$receiver')"); 
     mysqli_next_result($connection);
-   // $result2 = mysqli_query($connection, "CALL setNotification('$username', '$receiver', 0)");
+    $result2 = mysqli_query($connection, "CALL setNotification('$username', '$receiver', 0)");
     $array = array();
-    
+    $var = 0;
     
 
     while($row = mysqli_fetch_assoc($result)){
-        array_push($array, array($row["sender"], $row["msg"]));
+        if($var <= 5){
+            array_push($array, array($row["sender"], $row["msg"]));
 
+        }
+        $var += 1;
     }
+    
     
     echo json_encode($array);
 ?>  
